f19292f6e219982f4d72cc8f3aa107c41ab7fea0
silenciopz <silencioso_br@outlook.com>
2024-08-22 11:32:44 -0400

    adding ola.py

---
+++ ola.py
@@ -0,0 +1,10 @@
+def factorial(n):
+	if lf n == 0:
+		return;
+	else:
+		return n * factorial(n-1)
+		
+number = 5
+result = factorial(number)
+print(f"The factorial number of (number) is (result).")
+
